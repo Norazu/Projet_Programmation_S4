4a79d7e5b5e78e8764da59e67e42683dc0c8d2be
OGaudin <oscar.gaudin@gmail.com>
2023-12-14 16:16:57 +0100

    merge_master_et_jeu

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import Game from './Game';
 import Chat from './Chat';
 import reportWebVitals from './reportWebVitals';
 
@@ -9,7 +10,8 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App/>
+    <Game/>
     <Chat/>
   </React.StrictMode>
 );
