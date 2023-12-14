01421dc3c4958194571c3af94c2f64ff68ccb65d
OGaudin <oscar.gaudin@gmail.com>
2023-12-14 15:49:28 +0100

    page_principale_de_jeu

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const app = express();
 const http = require("http");
 const { Server } = require("socket.io");
 const cors = require("cors");
+const { time } = require("console");
 
 app.use(cors());
 
@@ -15,6 +16,9 @@ const io = new Server(server, {
   },
 });
 
+var playersList = [["player1",10],["player2",5]];
+var cartes = {"001":["2_of_clubs","5_of_hearts","jack_of_spades"]};
+
 io.on("connection", (socket) => {
   console.log(`User Connected: ${socket.id}`);
 
@@ -24,7 +28,37 @@ io.on("connection", (socket) => {
 
   socket.on("newAccount", (nom,mdp) =>{
     
-  })
+  });
+
+  socket.on("getPlayers", () => {
+    socket.emit("playersList",playersList);
+  });
+
+  socket.on("getCards",playerId => {
+    socket.emit("cardsList",cartes[playerId]);
+  });
+
+  function timer(timeLeft) {
+    io.emit("timeLeft", timeLeft - 1);
+    return timeLeft - 1;
+  }
+
+  function startTimer(duration) {
+    const timerId = setInterval(() => {
+      duration = timer(duration);
+
+      if (duration <= 0) {
+        clearInterval(timerId);
+        io.emit("choosingEnd");
+      }
+    }, 1000);
+  }
+
+  socket.on("startTimer", () => {
+    startTimer(10);
+  });
+
+
 });
 
 server.listen(3001, () => {
