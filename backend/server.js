01217171e2627de80bf4fc75d00dfc160cccb142
Boureux Nathan <nathan.boureux@etu.umontpellier.fr>
2023-12-17 18:55:58 +0100

    Fonctionne presque

--- backend/server.js
+++ backend/server.js
@@ -71,6 +71,11 @@ io.on("connection", (socket) => {
   socket.on("hello", (sessId) => {
     if (joueursConnectes.includes(sessId)) {
       socket.emit("connected");
+      for (var pt in listeParties) {
+        if (listeParties[pt].listeJoueurs.includes(sessId)) {
+          rejoindrePartie(sessId, pt);
+        }
+      }
     } else {
       joueursConnectes.push(sessId);
     }
@@ -135,9 +140,8 @@ io.on("connection", (socket) => {
     }
   });
 
-  socket.on('creationPartie', async (type, nbMinJoueurs, nbMaxJoueurs, idCreateur) => {
-    codepartie = defCode();
-    listeParties[codepartie] = new partie(type, idCreateur, nbMinJoueurs, nbMaxJoueurs, 1, [idCreateur],0,0,{});
+  async function creerPartie(codepartie, type, nbMinJoueurs, nbMaxJoueurs, idCreateur, cartes) {
+    listeParties[codepartie] = new partie(type, idCreateur, nbMinJoueurs, nbMaxJoueurs, 1, [idCreateur],0,0,cartes);
     listeParties[codepartie].listeIdentifiants=[socket.id];
     await socket.join(codepartie.toString());
     console.log("partie " + codepartie + " créée");
@@ -145,17 +149,24 @@ io.on("connection", (socket) => {
       socket.emit("setGameId", codepartie.toString());
       socket.emit("playersList", listeParties[codepartie].listeJoueurs);
     });
+  }
+
+  socket.on('creationPartie', (type, nbMinJoueurs, nbMaxJoueurs, idCreateur) => {
+    codepartie = defCode();
+    creerPartie(codepartie, type, nbMinJoueurs, nbMaxJoueurs, idCreateur, {});
   });
 
-  socket.on("joinGame", async (idJoueur, idRoom) => {
+  async function rejoindrePartie(idJoueur, idRoom) {
     var idRoomInt = parseInt(idRoom);
     if (idRoomInt in listeParties) {
       if (listeParties[idRoomInt].status==0){
         if (listeParties[idRoomInt].nbJoueurs == listeParties[idRoomInt].nbMaxJoueurs) {
           socket.emit("roomComplete");
         } else {
-          listeParties[idRoomInt].nbJoueurs += 1;
-          listeParties[idRoomInt].listeJoueurs.push(idJoueur);
+          if (!listeParties[idRoomInt].listeJoueurs.includes(idJoueur)) {
+            listeParties[idRoomInt].nbJoueurs += 1;
+            listeParties[idRoomInt].listeJoueurs.push(idJoueur);
+          }
           listeParties[idRoomInt].listeIdentifiants.push(socket.id);
           await socket.join(idRoom);
           socket.emit('goToGame', idRoom, () => {
@@ -170,6 +181,10 @@ io.on("connection", (socket) => {
     } else {
       socket.emit("roomDontExist");
     }
+  }
+
+  socket.on("joinGame", (idJoueur, idRoom) => {
+    rejoindrePartie(idJoueur, idRoom);
   });
 
   socket.on('disconnecting', () => {
@@ -274,17 +289,16 @@ io.on("connection", (socket) => {
     }
   });
 
-  function score(playerId){
-    connexiondb.query("UPDATE joueurs SET score=score+1 WHERE pseudo="+"'"+playerId+"'",function (err,result){
-      if(err){
-        console.error('error on insertion: ' + err.stack);
-      }
-    })
-  }
-
-
-
   function pickWinner(gameId, cardsToWin) {
+    function score(playerId){
+      connexiondb.query("UPDATE joueurs SET score=score+1 WHERE pseudo='"+playerId+"'",function (err,result){
+        if(err){
+          console.error('error on insertion: ' + err.stack);
+        } else {
+          console.log("score of " + playerId + " updated");
+        }
+      })
+    }
     let winner = bataille[gameId][0][0];
     let otherWinners = [];
     let winnerCardValue = 1;
@@ -387,6 +401,36 @@ io.on("connection", (socket) => {
     } else{
       socket.emit("PasPermSauvegarde");
     }
+  });
+
+  socket.on("loadGame", (code) => {
+    var cartes = {};
+    connexiondb.query("SELECT * FROM parties AS pt JOIN partiejoueur AS ptj ON pt.idGame=ptj.idGame WHERE pt.idGame='" + code + "'", (err, result) => {
+      if (err) {
+        console.error('error on query: ' + err.stack);
+        return;
+      } else {
+        for (row of result) {
+          cartes[row.idJoueur] = row.carte.split("|");
+        }
+        creerPartie(row.idGame, result[0].typeJeu, result[0].nbMinJoueurs, result[0].nbMaxJoueurs, result[0].idCreateur, cartes);
+      }
+    });
+  });
+
+  socket.on("getSavedGames", () => {
+    var queryResult = []
+    connexiondb.query("SELECT * FROM parties", (err, result) => {
+      if (err) {
+        console.error('error on query: ' + err.stack);
+        return;
+      } else {
+        for (row of result) {
+          queryResult.push([row.idGame]);
+        }
+        socket.emit("returnSavedGames", queryResult);
+      }
+    });
   });
 
   socket.on('recuperationListeParties', (typeJeu) => {
--- src/Home.js
+++ src/Home.js
@@ -1,23 +1,30 @@
 import { useEffect, useState } from "react";
 import { socket } from "./socket.js";
 
-function joinGame() {
-  var identifiant = document.getElementById("idGame").value;
-  socket.emit("joinGame",localStorage.getItem("sessId"),identifiant);
-}
-
 function joinGameByList(identifiant){
   //console.log(typeof(identifiant));
   //console.log(identifiant);
   socket.emit("joinGame",localStorage.getItem("sessId"),identifiant);
 }
 
-function Parties({code}){
+function loadGameByList(code) {
+  socket.emit("loadGame", code);
+}
+
+function Parties({code, page}){
   return(
-      <div className="Parties">
-          <p>Code de la partie: {code}</p>
-          <button onClick={()=>joinGameByList(code)}>Rejoindre la partie</button>
-      </div>
+    <div className="Parties">
+      <p>Code de la partie : {code}</p>
+      {page ? (
+        <>
+        <button onClick={() => loadGameByList(code)}>Charger la partie</button>
+        </>
+      ) : (
+        <>
+        <button onClick={() => joinGameByList(code)}>Rejoindre la partie</button>
+        </>
+      )}
+    </div>
   );
 }
 
@@ -43,13 +50,47 @@ function ListeDesElements() {
   );
 }
 
+function PartiesSauvegardees() {
+  const [savedGames, setSavedGames] = useState([]);
+  const [savePage, setSavePage] = useState(false);
+
+  useEffect(() => {
+    socket.emit("getSavedGames");
+
+    socket.on("returnSavedGames", liste => {
+      setSavedGames(liste);
+      setSavePage(true);
+    });
+    return () => {
+      socket.off("returnSavedGames");
+    };
+  }, []);
+  return (
+    <div className="playerList">
+      {savePage && savedGames.map((partie) => (
+          <Parties code={partie[0]} page={savePage}/>
+      ))}
+    </div>
+  );
+}
+
 function Home() {
   const [showCreateGame, setShowCreateGame] = useState(false);
   const [showGameList, setShowGameList] = useState(false);
+  const [savedGames, setSavedGames] = useState(false);
 
   function joinGame() {
     var identifiant = document.getElementById("idGame").value;
     socket.emit("joinGame",localStorage.getItem("sessId"),identifiant);
+  }
+
+  function loadGame() {
+    var code = document.getElementById("loadGame").value;
+    socket.emit("loadGame", code);
+  }
+
+  function toSavedGames() {
+    setSavedGames(true);
   }
 
   function afficherCreationPartie() {
@@ -92,33 +133,48 @@ function Home() {
   return (
     <div className="Home">
       <button id="deco" onClick={deconnexion}>Se déconnecter</button>
-      <div className="Container1">
-        <button type="button" onClick={afficherCreationPartie}>Afficher le formulaire de création de partie</button>
-        {showCreateGame && (
-          <div>
-            {/* Contenu de la page de création de partie */}
-            <label htmlFor="choixTypeJeu">A quel jeu voulez-vous jouer ? </label>
-            <select id="choixTypeJeu">
-              <option>Bataille ouverte</option>
-            </select>
-            <button type="button" onClick={creationPartie}>Créer la partie</button>
-          </div>
-        )}
-        <input id="idGame" type="text" placeholder="Identifiant de la partie" />
-        <button onClick={joinGame}>Rejoindre la partie</button>
-      </div>
-      <div className="Container1">
-        {!showGameList && (
-        <button type="button" onClick={afficherListeParties}> Afficher la liste des parties</button>
-        )}
-        {showGameList && (
+        {savedGames ? (
+          <>
           <div className="Container1">
-            {/* Contenu de la liste des parties */}
-            <p>Parties disponibles</p>
-            <ListeDesElements />
+            <input id="loadGame" type="text" placeholder="Code de la partie sauvegardée" />
+            <button type="button" onClick={loadGame}>Charger la partie</button>
           </div>
+          <div className="Container1">
+            <PartiesSauvegardees />
+          </div>
+          </>
+        ) : (
+          <>
+          <div className="Container1">
+            <button type="button" onClick={toSavedGames}>Charger une partie sauvegardée</button>
+            <button type="button" onClick={afficherCreationPartie}>Créer une partie</button>
+            {showCreateGame && (
+              <div>
+                {/* Contenu de la page de création de partie */}
+                <label htmlFor="choixTypeJeu">A quel jeu voulez-vous jouer ? </label>
+                <select id="choixTypeJeu">
+                  <option>Bataille ouverte</option>
+                </select>
+                <button type="button" onClick={creationPartie}>Créer la partie</button>
+              </div>
+            )}
+            <input id="idGame" type="text" placeholder="Identifiant de la partie" />
+            <button onClick={joinGame}>Rejoindre la partie</button>
+          </div>
+          <div className="Container1">
+            {!showGameList && (
+            <button type="button" onClick={afficherListeParties}> Afficher la liste des parties</button>
+            )}
+            {showGameList && (
+              <div className="Container1">
+                {/* Contenu de la liste des parties */}
+                <p>Parties disponibles</p>
+                <ListeDesElements />
+              </div>
+            )}
+          </div>
+          </>
         )}
-      </div>
     </div>
   );
 }
